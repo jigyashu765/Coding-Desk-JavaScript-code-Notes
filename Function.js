dde79b538dad043e5225adcb9fa40a3952a49ee1
Jigyashu Kumar Gupta <75210289+jigyashu765@users.noreply.github.com>
2024-10-08 20:43:48 +0530

    Add files via upload

---
+++ Function.js
@@ -0,0 +1,51 @@
+// Function Declaration
+
+// function funName(){
+//     // task of this function;
+// }
+
+// funName();
+
+// function greet(){
+//     console.log("Hello Coding Desk");
+// }
+
+// for(var i = 0; i < 10; i++){
+//     greet();
+// }
+
+
+// Function Parameters
+
+// function greet(name){
+//     console.log("Hello " + name);
+// }
+
+// greet("Jigyashu");
+// greet("Kumar");
+// greet("Gupta");
+
+
+// Write a program to print the sq. of numbers given in Array using function with paramerts, for loop and Exponentiation to find sq.;
+// function square(num){
+//     console.log(num ** 2); // num^2
+// }
+
+
+// // var arr = [1^2 = 1,2^2 = 4,3^2 = 9,4^2 = 16];
+// var arr = [1,2,3,4];
+
+// for(var i=0; i< arr.length; i++){
+//    square(arr[i]);
+// }
+
+
+// Return Value
+
+function add(a, b){
+    return(a+b);
+}
+
+
+var res = add(2,5); // 7
+console.log(res);
